40749cff1cf289f5ff224a33bc2ac814dcf31958
dangnha <nhand.21it@vku.udn.vn>
2023-06-09 23:34:43 +0700

    Init project

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./App.scss";
+import Header from "./components/Header";
+import TableUsers from "./components/TableUsers";
+
+function App() {
+  return (
+    <div className="App-container">
+      <Header />
+      <TableUsers />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/TableUsers.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const TableUsers = () => {
+  return <div>TableUsers</div>;
+};
+
+export default TableUsers;
