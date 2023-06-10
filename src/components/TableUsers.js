d60337c10f39751007281b12dbd627b21867cce7
dangnha <nhand.21it@vku.udn.vn>
2023-06-10 23:46:39 +0700

    Get User and Paginate

--- src/components/TableUsers.js
+++ src/components/TableUsers.js
@@ -1,7 +1,82 @@
+import Table from "react-bootstrap/Table";
 import React from "react";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { FetchAllUser } from "../services/UserServices";
+import ReactPaginate from "react-paginate";
 
 const TableUsers = () => {
-  return <div>TableUsers</div>;
+  const [listUsers, setListUsers] = useState([]);
+  const [totalPages, setTotalPages] = useState(0);
+  const [itemsPerPage, setItemsPerPage] = useState(0);
+
+  useEffect(() => {
+    getUsers(1);
+  }, []);
+
+  const getUsers = async (page) => {
+    let res = await FetchAllUser(page);
+    console.log(">>> Check:", res);
+    if (res && res.data) {
+      setListUsers(res.data);
+      setTotalPages(res.total_pages);
+      setItemsPerPage(res.per_page);
+    }
+  };
+
+  const handlePageClick = (event) => {
+    getUsers(+event.selected + 1);
+  };
+
+  return (
+    <div>
+      <Table striped bordered hover>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>First Name</th>
+            <th>Last Name</th>
+            <th>Email</th>
+          </tr>
+        </thead>
+        <tbody>
+          {listUsers &&
+            listUsers.length > 0 &&
+            listUsers.map((user) => {
+              return (
+                <tr key={`user-${user.id}`}>
+                  <td>{user.id}</td>
+                  <td>{user.email}</td>
+                  <td>{user.first_name}</td>
+                  <td>{user.last_name}</td>
+                </tr>
+              );
+            })}
+        </tbody>
+      </Table>
+      <ReactPaginate
+        breakLabel="..."
+        nextLabel="next >"
+        onPageChange={handlePageClick}
+        pageRangeDisplayed={3}
+        pageCount={totalPages}
+        previousLabel="< previous"
+        renderOnZeroPageCount={null}
+        marginPagesDisplayed={2}
+        pageClassName="page-item"
+        pageLinkClassName="page-link"
+        previousClassName="page-item"
+        previousLinkClassName="page-link"
+        nextClassName="page-item"
+        nextLinkClassName="page-link"
+        breakLabel="..."
+        breakClassName="page-item"
+        breakLinkClassName="page-link"
+        containerClassName="pagination"
+        activeClassName="active"
+      />
+    </div>
+  );
 };
 
 export default TableUsers;
